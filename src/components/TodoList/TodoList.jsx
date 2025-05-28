6a7990aa4d1da2910b878b38b451d1b74bcb1ffb
DELL <sangyeop0529@gmail.com>
2025-05-28 10:50:54 +0900

    todo 버튼 추가

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
     { id: "123", text: "공부하기", status: "active" },
     { id: "124", text: "청소하기", status: "active" },
   ]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
   return (
-    <div>
-      {todos.map((item) => (
-        <li key={item.key}>{item.text}</li>
-      ))}
-    </div>
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.key}>{item.text}</li>
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
   );
 };
 
